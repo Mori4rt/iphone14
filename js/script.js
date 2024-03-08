b030aa48bf292ff98203522cd7353fc13d9dbb24
Madina <madina2005pulotova@gmail.com>
2024-03-09 02:28:08 +0700

    adaptiv

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.menu__btn');
+const nav = document.querySelector('.nav');
+
+
+btn.addEventListener('click', () => {
+    nav.classList.toggle('menu__active');
+})
